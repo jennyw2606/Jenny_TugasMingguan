9209b286a029cc0a8aa7e8184a956b8fb9271256
jennyw2606 <jennywulandari2606@gmail.com>
2025-10-09 20:04:27 +0700

    edit web

---
+++ resources/views/singleberita.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@add($new_berita)
+@section('title', 'Detail Berita - MyApp')
+@section('content')
+<article>
+    <h1>Judul</h1>
+    <h3>Penulis</h3>
+    <p>Isi berita lengkap...</p>
+</article>
--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,59 @@ Route::get('/profile', function () {
     return view('profile');
 });
 
-Route::get('/berita', function () {
-    return view('berita');
+Route::get('/berita/{slug}', function ($slugp) {
+
+    $data_berita = [
+        "judul" => "Unimus Jaya",
+        "slug" => "unimus-jaya",
+        "penulis" => "Jenny W",
+        "konten" => "Unimus Bergembira",
+    ];
+
+    [
+    "judul" => "Berita Terkini",
+    "slug"  => "berita-terkini",
+    "penulis" => "Hafisa",
+    "konten" => "Mukbang 20 KG Ayam Geprek",
+    ];
+
+    [
+    "judul" => "Berita Viral",
+    "slug"  => "berita-viral",
+    "penulis" => "Amar",
+    "konten" => "Unimus Juara 1 Lomba",
+    ];
+
+
+    $new_berita = [];
+
+    foreach ($data_berita as $berita) 
+    {
+    if ($berita["slug"] === $slugp) 
+    {
+        $new_berita = $berita;
+    }
+    }
+
+    return view('singleberita', [
+    "title" => "Berita",
+    "new_berita" => $new_berita,
+]);
 });
 
+
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', [
+        "title" => "Contact",
+    ]);
 });
+
+Route::get('/mahasiswa', function (): View {
+    return view('mahasiswa');
+});
+
+Route::get('/dosen', function () {
+    return view('dosen');
+});
+
+
